bbc497a0bb134d226a0a33a63a3395bd7cc986d6
halfwit <michaelmisch1985@gmail.com>
2022-06-02 23:03:41 -0700

    Add in some buffer boilerplate

--- lib/views/buffers.dart
+++ lib/views/buffers.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:altid/src/buffer.dart';
-import 'package:altid/src/service.dart';
+//import 'package:altid/src/service.dart';
+import 'package:altid/views/menu.dart' as menu;
 
 class CreateWidget extends StatelessWidget {
   const CreateWidget({Key? key}) : super(key: key);
@@ -8,13 +9,42 @@ class CreateWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final arguments = ModalRoute.of(context)!.settings.arguments as Set;
-    Service service = arguments.first;
+    //Service service = arguments.first;
     Buffer buffer = arguments.last;
+    TextEditingController _inputController = TextEditingController();
 
     return Scaffold(
+      drawer: menu.build(),
       appBar: AppBar(
         title: Text(buffer.name),
       ),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(5),
+              scrollDirection: Axis.vertical,
+              // Probably becomes a stream instead, since we want subs for data changes
+              child: Text(
+                buffer.main
+              ),
+            ),
+          ),
+          TextField(
+            onSubmitted: (String value) {
+              buffer.Write(value);
+              _inputController.clear();
+            },
+            autofocus: true,
+            controller: _inputController,
+            textInputAction: TextInputAction.newline,
+            textAlignVertical: TextAlignVertical.center,
+            decoration: const InputDecoration(
+              prefixIcon: Icon(Icons.arrow_right),
+            ),
+          ),
+        ], 
+      )
     );
   }
 }
